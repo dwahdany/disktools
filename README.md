959e502e474e0340ab7b738fe1d9d936e27ffcb9
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2015-01-23 16:18:57 +0100

    modified:   README.md modified:   bin/duc modified:   setup.py modified:   src/disktools/__init__.py - Rolling out the first version.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # disktools
-Extensions to basic disk tools like du, df, etc, but written in python
+Extensions to basic disk tools like du, df, etc, but written in python.
 
 ---
 
--- setup.py
+++ setup.py
@@ -20,25 +20,36 @@ def readme():
 def get_version():
     src_path = os.path.join(os.path.dirname(__file__), 'src')
     sys.path = [src_path] + sys.path
-    import findtools
-    findtools.__path__
-    return findtools.__version__
+    import disktools
+    disktools.__path__
+    return disktools.__version__
 
 
 setup(
-    name='findtools',
+    name='disktools',
     version=get_version(),
-    description='Extensions to basic disk tools like du, df, etc, but written in python',
+    description='Extensions to basic disk tools like du, df, etc, but '
+                'written in python',
     long_description=readme(),
     author='Yauhen Yakimovich',
     author_email='eugeny.yakimovitch@gmail.com',
     url='https://github.com/ewiger/disktools',
     license='MIT',
-    #scripts=glob('bin/*'),
-    #data_files=glob('libexec/*'),
-    packages=['findtools'],
-    package_dir={
-        'disktools': 'src/disktools',
-    },
+    scripts=['bin/duc'],
+    # data_files=glob('libexec/*'),
+    packages=['disktools'],
+    package_dir={'': 'src'},
     download_url='https://github.com/ewiger/disktools/tarball/master',
+    install_requires=[
+        'argparse >= 1.1',
+        'bson >= 0.3.3',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+    ],
 )
